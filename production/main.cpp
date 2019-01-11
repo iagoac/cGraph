fb03861914662f143ebdcc57b768339c1a477f68
Iago Carvalho <iago.august@gmail.com>
2019-01-11 15:06:41 -0200

    Node and edge counters turned private in cGraph class

--- production/main.cpp
+++ production/main.cpp
@@ -4,12 +4,21 @@
 int main(int argc, char* const* argv) {
   Graph g;
   for (int i = 0; i < 10; i++) {
-    g.add_node();
+    g.add_node(i);
   }
 
   g.print_nodes();
 
-  g.add_edge(0,2);
+  g.add_edge(0,2,5);
+  g.add_edge(2,1,3);
+  g.add_edge(2,4,1);
+
+  std::cout << g.nodes() << " " << g.edges() << std::endl;
+
+  // std::cout << g.edge[0][2] << " " << g.edge[3][5] << std::endl;
+  for (auto i : g.neighboors_list[2]) {
+    std::cout << i << std::endl;
+  }
 
   // std::cout << node
 }
--- release/cgraph.hpp
+++ release/cgraph.hpp
@@ -7,8 +7,8 @@
 template <typename T = int, typename P = int>
 class Graph {
 private:
-  int nodes = 0;
-  int edges = 0;
+  int nodes_count = 0;
+  int edges_count = 0;
 
 public:
   Graph(){};
@@ -17,28 +17,44 @@ public:
   std::vector<std::vector<P>> edge;
   std::vector<std::list<int>> neighboors_list;
 
+  int nodes() {
+    return this->nodes_count;
+  }
+
+  int edges() {
+    return this->edges_count;
+  }
+
   void add_node() {
     this->node.push_back(static_cast<T> (0));
-    nodes++;
+    this->nodes_count++;
   }
 
   void add_node(T new_node) {
     this->node.push_back(new_node);
-    this->nodes++;
 
-    std::vector<P> v;
+    P aux;
+    for (int i = 0; i < this->edge.size(); i++) {
+      this->edge[i].push_back(aux);
+    }
+
+    this->nodes_count++;
+    std::vector<P> v (this->nodes());
     this->edge.push_back(v);
+
+    std::list<int> aux_list;
+    this->neighboors_list.push_back(aux_list);
   }
 
   void remove_last_node() {
     this->node.pop_back();
-    this->nodes--;
+    this->nodes_count--;
   }
 
   void remove_all_nodes() {
     this->remove_all_edges();
     this->node.resize(0);
-    this->nodes = 0;
+    this->nodes_count = 0;
   }
 
   void print_nodes() {
@@ -48,24 +64,24 @@ public:
   }
 
   void add_edge(int i, int j) {
-    if (i < this->nodes && j < this->nodes && i >= 0 && j >= 0) {
+    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
       this->edge[i][j] = static_cast<P> (0);
       this->edge[j][i] = static_cast<P> (0);
       this->neighboors_list[i].push_back(j);
       this->neighboors_list[j].push_back(i);
-      this->edges++;
+      this->edges_count++;
     } else {
       std::cout << "Nodes does not exist" << std::endl;
     }
   }
 
   void add_edge(int i, int j, P value) {
-    if (i < this->nodes && j < this->nodes && i >= 0 && j >= 0) {
+    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
       this->edge[i][j] = value;
       this->edge[j][i] = value;
       this->neighboors_list[i].push_back(j);
       this->neighboors_list[j].push_back(i);
-      this->edges++;
+      this->edges_count++;
     } else {
       std::cout << "Nodes does not exist" << std::endl;
     }
@@ -79,17 +95,17 @@ public:
       }
     }
 
-    if (i < this->nodes && j < this->nodes && i >= 0 && j >= 0) {
+    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
       this->neighboors_list[i].remove(j);
       this->neighboors_list[j].remove(i);
-      this->edges--;
+      this->edges_count--;
     } else {
       std::cout << "Nodes does not exist" << std::endl;
     }
   }
 
   void remove_all_edges() {
-    for (int i = 0; i < this->nodes; i++) {
+    for (int i = 0; i < this->nodes(); i++) {
       this->neighboors_list[i].clear();
     }
     this->edges = 0;
