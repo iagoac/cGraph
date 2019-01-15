3a2ea52868d83784996ab0b4adb6c946ad93d35a
Iago Carvalho <iago.august@gmail.com>
2019-01-15 15:52:18 -0200

    BaseGraph class created; Graph and Digraph now extends BaseGraph

--- release/cgraph.hpp
+++ release/cgraph.hpp
@@ -6,17 +6,19 @@
 #include <vector>
 #include <list>
 #include <iostream>
-#include <fstream>
+
+/* ------------------------------------
+     Basic graph class implementation
+   ------------------------------------ */
 
 template <typename T = int, typename P = int>
-class Graph {
-private:
+class BaseGraph {
+protected:
   int nodes_count = 0;
   int edges_count = 0;
 
 public:
-  Graph(){};
-
+  BaseGraph(){};
   std::vector<T> node;
   std::vector<std::vector<P>> edge;
   std::vector<std::list<int>> neighboors_list;
@@ -29,9 +31,8 @@ public:
     return this->edges_count;
   }
 
-  void add_node() {
-    this->node.push_back(static_cast<T> (0));
-    this->nodes_count++;
+  int num_arcs() {
+    return this->edges_count;
   }
 
   void add_node(T new_node) {
@@ -43,11 +44,15 @@ public:
     }
 
     this->nodes_count++;
-    std::vector<P> v (this->nodes());
+    std::vector<P> v (this->num_nodes());
     this->edge.push_back(v);
 
     std::list<int> aux_list;
     this->neighboors_list.push_back(aux_list);
+  }
+
+  void add_node() {
+    this->add_node(static_cast<T> (0));
   }
 
   void remove_last_node() {
@@ -67,31 +72,23 @@ public:
     }
   }
 
-  void add_edge(int i, int j) {
-    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
-      this->edge[i][j] = static_cast<P> (0);
-      this->edge[j][i] = static_cast<P> (0);
-      this->neighboors_list[i].push_back(j);
-      this->neighboors_list[j].push_back(i);
-      this->edges_count++;
-    } else {
-      std::cout << "Nodes does not exist" << std::endl;
-    }
-  }
-
-  void add_edge(int i, int j, P value) {
-    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
+  void add_arc(int i, int j, P value) {
+    if (i < this->num_nodes() && j < this->num_nodes() && i >= 0 && j >= 0) {
       this->edge[i][j] = value;
-      this->edge[j][i] = value;
+      // this->edge[j][i] = value;
       this->neighboors_list[i].push_back(j);
-      this->neighboors_list[j].push_back(i);
+      // this->neighboors_list[j].push_back(i);
       this->edges_count++;
     } else {
       std::cout << "Nodes does not exist" << std::endl;
     }
   }
 
-  void remove_edge(int i, int j) {
+  void add_arc(int i, int j) {
+    this->add_arc(i, j, static_cast<P> (0));
+  }
+
+  void remove_arc(int i, int j) {
     for (int k : this->neighboors_list[i]) {
       if (k == j) {
         std::cout << "Edge does not exists!" << std::endl;
@@ -99,32 +96,59 @@ public:
       }
     }
 
-    if (i < this->nodes() && j < this->nodes() && i >= 0 && j >= 0) {
+    if (i < this->nodes_count && j < this->nodes_count && i >= 0 && j >= 0) {
       this->neighboors_list[i].remove(j);
-      this->neighboors_list[j].remove(i);
       this->edges_count--;
     } else {
       std::cout << "Nodes does not exist" << std::endl;
     }
   }
 
+};
+
+
+/* ------------------------------
+     Graph class implementation
+   ------------------------------ */
+template <typename T = int, typename P = int>
+class Graph: public BaseGraph<T, P> {
+public:
+  Graph(){};
+
+  void add_edge(int i, int j) {
+    this->add_arc(i, j);
+    this->add_arc(j, i);
+    this->edges_count--;
+  }
+
+  void add_edge(int i, int j, P value) {
+    this->add_arc(i, j, value);
+    this->add_arc(j, i, value);
+    this->edges_count--;
+  }
+
+
+  void remove_edge(int i, int j) {
+    this->remove_arc(i, j);
+    this->remove_arc(j, i);
+    this->edges_count++;
+  }
+
   void remove_all_edges() {
     for (int i = 0; i < this->nodes(); i++) {
       this->neighboors_list[i].clear();
     }
-    this->edges = 0;
+    this->edges_count = 0;
   }
 };
 
-// template <typename T = int>
-// class Digraph {
-//   int nodes = 0;
-//   int arcs = 0;
-//
-//   std::vector<T> node;
-//
-// public:
-//   Digraph(){};
-// };
+/* --------------------------------
+     Digraph class implementation
+   -------------------------------- */
+template <typename T = int, typename P = int>
+class Digraph: public BaseGraph<T, P> {
+public:
+  Digraph(){};
+};
 
 #endif // CGRAPH_RELEASE_CGRAPH_H
