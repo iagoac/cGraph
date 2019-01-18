fe3224ea3e935af69ee0b37bcf26f8dfe4cefec9
Iago Carvalho <iago.august@gmail.com>
2019-01-18 16:11:02 -0200

    Parenthesis fault. I really need to create an unit test for this class :(

--- release/cgraph.hpp
+++ release/cgraph.hpp
@@ -110,7 +110,7 @@ public:
   }
 
   void add_edge(int i, int j) { this->add_arc(i,j); }
-  void add_edge(int i, int j, P value { this->add_arc(i, j, value); }
+  void add_edge(int i, int j, P value) { this->add_arc(i, j, value); }
   void remove_edge(int i, int j) { this->remove_arc(i, j); }
 };
 
