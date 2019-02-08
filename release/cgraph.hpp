a0cfb6f195df3609cdbb9f172af4eed02c22ac64
Iago Carvalho <iago.august@gmail.com>
2019-02-08 07:44:19 -0200

    Introduced a structure to store terminals

--- release/cgraph.hpp
+++ release/cgraph.hpp
@@ -14,7 +14,6 @@
 template <typename T = int, typename P = int>
 class Digraph {
 protected:
-  int nodes_count = 0;
   int edges_count = 0;
 
 public:
@@ -25,18 +24,13 @@ public:
   std::vector<std::list<int>> in_node;
   std::vector<std::list<int>> out_node;
   std::vector<int> terminals;
+  std::vector<int> terminal_list;
 
-  int num_nodes() {
-    return this->nodes_count;
-  }
+  int num_nodes() { return this->node.size(); }
 
-  int num_edges() {
-    return this->edges_count;
-  }
+  int num_edges() { return this->edges_count; }
 
-  int num_arcs() {
-    return this->edges_count;
-  }
+  int num_arcs() { return this->edges_count; }
 
   void add_node(T new_node) {
     this->node.push_back(new_node);
@@ -47,7 +41,6 @@ public:
       this->edge[i].push_back(aux);
     }
 
-    this->nodes_count++;
     std::vector<P> v (this->num_nodes());
     this->edge.push_back(v);
 
@@ -68,7 +61,8 @@ public:
     this->adjacency_list.resize(0);
     this->in_node.resize(0);
     this->out_node.resize(0);
-    this->nodes_count = 0;
+    this->terminals.resize(0);
+    this->terminal_list.resize(0);
   }
 
   void print_nodes() {
@@ -101,7 +95,7 @@ public:
       }
     }
 
-    if (i < this->nodes_count && j < this->nodes_count && i >= 0 && j >= 0) {
+    if (i < this->num_nodes() && j < this->num_nodes() && i >= 0 && j >= 0) {
       this->neighboors_list[i].remove(j);
       this->in_node[j].remove(i);
       this->out_node[i].remove(j);
@@ -115,7 +109,8 @@ public:
   void add_edge(int i, int j, P value) { this->add_arc(i, j, value); }
   void remove_edge(int i, int j) { this->remove_arc(i, j); }
   bool is_terminal(int i) { return (this->terminals[i]); }
-  void set_terminal(int i) { this->terminals[i] = 1; }
+  void set_terminal(int i) { this->terminals[i] = 1; this->terminal_list.push_back(i); }
+  int num_terminals(void) { return (this->terminal_list.size()); }
 };
 
 
@@ -127,16 +122,22 @@ class Graph: public Digraph<T, P> {
 public:
   Graph(){};
 
-  void add_edge(int i, int j) {
-    this->Digraph<T,P>::add_arc(i, j);
-    this->Digraph<T,P>::add_arc(j, i);
-    this->edges_count--;
-  }
-
   void add_edge(int i, int j, P value) {
     this->Digraph<T,P>::add_arc(i, j, value);
     this->Digraph<T,P>::add_arc(j, i, value);
+
+    /* Needed to keep the in_node and out_node as in the
+    instance file description. This is needed for not
+    duplicating some constraints along with the model */
+    this->in_node[i].pop_back();
+    this->out_node[j].pop_back();
+
+    /* Remove one edge from the counter, for obvious reasons */
     this->edges_count--;
+  }
+
+  void add_edge(int i, int j) {
+    this->add_edge(i, j, static_cast<P> (0));
   }
 
   void remove_edge(int i, int j) {
