7585344d5abe3c258e0e2b795dbe04c9f27c4aac
Iago Carvalho <iago.august@gmail.com>
2019-07-16 11:20:05 -0300

    Dijkstra algoritm implemented. However, its only adapted to interval data edges... Must solve this issue

--- release/cgraph.hpp
+++ release/cgraph.hpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <list>
+#include <queue>
 #include <iostream>
 
 /* --------------------------------
@@ -117,6 +118,44 @@ public:
   bool is_terminal(int i) { return (this->terminals[i]); }
   void set_terminal(int i) { this->terminals[i] = 1; this->terminal_list.push_back(i); }
   int num_terminals(void) { return (this->terminal_list.size()); }
+
+  /*********************************************************************
+  ********************** Shortest path algorithms **********************
+  *********************************************************************/
+  std::vector<int> dijkstra(double multiplier) {
+    /* Creating the Dijktra's predefined and distance vectors */
+    std::vector<double> dist(this->num_nodes(), std::numeric_limits<double>::max());
+    std::vector<int>    prev(this->num_nodes(), -1);
+    dist[0] = 0.0;
+
+    /* Priority queue of nodes that were not yet analyzed
+    Queue uses a std::pair<int, int> where the first int
+    is the distance and the second is the node number */
+    std::priority_queue<std::pair<int, int>,
+                        std::vector<std::pair<int,int>>,
+                        std::greater<std::pair<int,int>>> q;
+    q.emplace(dist[0], 0);
+
+    while (!q.empty()) {
+      /* Gets the node with minimum distance */
+      int next = q.top().second;
+      q.pop();
+
+      /* For all of its neigboors
+      make the dijkstra relaxation step */
+      for (auto j : this->out_node[next]) {
+        int new_dist = dist[next] + this->edge[next][j].lower +
+                       multiplier * (this->edge[next][j].upper - this->edge[next][j].lower);
+        if (dist[j] > new_dist) {
+          prev[j] = next;
+          dist[j] = new_dist;
+          q.emplace(dist[j], j);
+        }
+      }
+    }
+    /* Returns the shortest path tree */
+    return (prev);
+  }
 };
 
 
